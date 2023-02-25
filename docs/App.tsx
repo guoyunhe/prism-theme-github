e6b086b8f1cea2f71cbce36df8f2bab6247aafae
Guo Yunhe <i@guoyunhe.me>
2023-02-25 17:48:56 +0800

    dark theme

--- docs/App.tsx
+++ docs/App.tsx
@@ -1,6 +1,8 @@
-import { BaseStyles } from '@primer/react';
 import Highlight, { defaultProps, Language } from 'prism-react-renderer';
-import '../github-light.css';
+import { useState } from 'react';
+import darkTheme from '../github-dark.css?raw';
+import lightTheme from '../github-light.css?raw';
+import './App.css';
 import codeTest from './code-test.md?raw';
 
 function findCode(language: string) {
@@ -22,33 +24,45 @@ const codeMap: Partial<Record<Language, string>> = {
 };
 
 export default function App() {
+  const [nightMode, setNightMode] = useState(false);
   return (
-    <BaseStyles>
-      <div style={{ background: '#fff', padding: 20 }}>
-        {Object.entries(codeMap).map(([language, code]) => (
-          <div key={language}>
-            <h2>{language}</h2>
-            <Highlight
-              {...defaultProps}
-              code={code.trim()}
-              language={language as Language}
-              theme={undefined} // disable default theme
-            >
-              {({ className, style, tokens, getLineProps, getTokenProps }) => (
-                <pre className={className} style={style}>
-                  {tokens.map((line, i) => (
-                    <div key={i} {...getLineProps({ line, key: i })}>
-                      {line.map((token, key) => (
-                        <span key={key} {...getTokenProps({ token, key })} />
-                      ))}
-                    </div>
-                  ))}
-                </pre>
-              )}
-            </Highlight>
-          </div>
-        ))}
-      </div>{' '}
-    </BaseStyles>
+    <div className={'app ' + (nightMode ? 'dark' : 'light')}>
+      <style>{nightMode ? darkTheme : lightTheme}</style>
+      <label>
+        Dark Mode:
+        <input
+          type="checkbox"
+          checked={nightMode}
+          onChange={(e) => {
+            setNightMode(e.target.checked);
+          }}
+        />
+      </label>
+
+      <h1>Code Test</h1>
+      {Object.entries(codeMap).map(([language, code]) => (
+        <div key={language}>
+          <h2>{language}</h2>
+          <Highlight
+            {...defaultProps}
+            code={code.trim()}
+            language={language as Language}
+            theme={undefined} // disable default theme
+          >
+            {({ className, style, tokens, getLineProps, getTokenProps }) => (
+              <pre className={className} style={style}>
+                {tokens.map((line, i) => (
+                  <div key={i} {...getLineProps({ line, key: i })}>
+                    {line.map((token, key) => (
+                      <span key={key} {...getTokenProps({ token, key })} />
+                    ))}
+                  </div>
+                ))}
+              </pre>
+            )}
+          </Highlight>
+        </div>
+      ))}
+    </div>
   );
 }
